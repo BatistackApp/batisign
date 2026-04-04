82f0b60225dcaaf48152ec70a6ece657d84957ab
maxim <maximemockelyn8@gmail.com>
2026-04-04 23:06:44 +0100

    feat(deps): Ajout de la dépendance opcodesio/log-viewer
    Cette dépendance fournit une interface pour visualiser les logs de
    l'application.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
+        LogViewer::auth(function ($request) {
+            return str_ends_with($request->user()->email, '@admin.com');
+        });
     }
 
     /**
